b43215b19eb16e741a1693cf6a852a1011d7fb35
zenithshrestha37 <zenithshrestha37@gmail.com>
2021-07-05 12:58:42 -0500

    first commit for backend1

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require ("express")
+
+const cors = require("cors")
+
+const app = express ()
+app.use(cors())
+app.use(express.json())
+
+app.get("/api/users", (req, res) => {
+    let friends = ["Zenith", "Smit", "Sai", "Anjali", "Karma"]
+    res.status(200).send(friends)
+})
+
+app.get("/weather/:temperature", (req,res)=> {
+    const phrase ='<h3> It was ${req.params.temperature} today</h3>';
+    res.status(200).send(phrase);
+})
+
+app.get("/weather/:temperature", (req,res) => {
+    const { temperature } = req.params
+    const phrase = '<h3> It was ${temperature} today </h3>'
+    res.status(200).send(phrase)
+})
+
+app.listen(4000, ()=> {
+    console.log('Serving running on port 4000')
+})
+
